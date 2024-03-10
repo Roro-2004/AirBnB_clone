8134e257c186290741f43e516a16e28806fa29e5
Roro-2004 <roaa.m.lotfy@gmail.com>
2024-03-10 23:08:24 +0200

    task4

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,18 @@ class BaseModel:
     Defines all common attributes/methods
     for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         intialize the class attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        format = '%Y-%m-%dT%H:%M:%S.%f'
+        if(len(kwargs) != 0):
+            self.id = kwargs['id']
+            self.created_at = datetime.strptime(kwargs['created_at'], format)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """
