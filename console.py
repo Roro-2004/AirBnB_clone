bbd29075d9a5cf72f4963f554ba37dfc23a4b1ab
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-03-12 20:44:41 +0200

    Mango ;)

--- console.py
+++ console.py
@@ -6,24 +6,24 @@ AirBnB Console Class
 
 
 class HBNBCommand(cmd.Cmd):
+
     """
     The command prompot interpreter
+    Attributes:
+        prompt:  prompt string
     """
     prompt = "(hbnb)"
+
     def do_quit(self, args):
         """
         to quit the program
         """
         return True
 
-
-
     def do_EOF(self, arg):
         """exit the program"""
         return True
-    
-    #def emptyline(self):
-        #return False
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
